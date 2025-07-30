1ad6b14bde0369f5f8e6fa9c1fe228874810c84b
Anderson <anderson.rangel@red-gate.com>
2025-07-30 22:58:49 +1000

    fsf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-
+# Start SQL Server in background
 /opt/mssql/bin/sqlservr &
 
-echo "Waiting for SQL Server to start..."
+# Wait until it's ready to accept connections
+echo "Waiting for SQL Server to be ready..."
 for i in {1..30}; do
-  /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -Q "SELECT 1" &>/dev/null
+  /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Redg@te1' -Q "SELECT 1" &>/dev/null
   if [ $? -eq 0 ]; then
-    echo "SQL Server is ready."
+    echo "SQL Server is ready!"
     break
   fi
-  sleep 2
+  sleep 5
 done
 
-echo "Running init script..."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -i /tmp/createdb.sql
+# Run the initialization script
+echo "Running createdb.sql..."
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Redg@te1' -i /tmp/createdb.sql
 
+# Keep container alive
 wait
