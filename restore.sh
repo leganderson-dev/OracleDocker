4dfc6b9659659e3f2a82eddd41877408be1f1531
Anderson <anderson.rangel@red-gate.com>
2025-07-30 21:49:08 +1000

    restore.sh

---
+++ restore.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+/opt/mssql/bin/sqlservr &
+
+echo "⏳ Waiting for SQL Server to start..."
+sleep 30
+
+echo "🔄 Restoring database..."
+sqlcmd -S localhost -U SA -P 'Redg@te1' \
+  -Q "RESTORE DATABASE SSC_Dev FROM DISK = '/var/opt/mssql/backup/SSC.bak' WITH REPLACE, MOVE 'SSC' TO '/var/opt/mssql/data/SSC_Dev.mdf', MOVE 'SSC_log' TO '/var/opt/mssql/data/SSC_Dev_log.ldf'"
+
+tail -f /dev/null
